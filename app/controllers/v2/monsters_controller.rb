8fea53d4c0e6d96683d7bce5115f2cbead41d8b7
jeffreybiles <bilesjeffrey@gmail.com>
2016-05-29 13:56:27 -0500

    monsters api for the crud 2016 app

---
+++ app/controllers/v2/monsters_controller.rb
@@ -0,0 +1,5 @@
+class V2::MonstersController < ApplicationController
+  def resource_class_name
+    'monster'
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,10 @@ Rails.application.routes.draw do
   resources :team_memberships
   resources :sessions, only: [:create]
   post 'token' => 'sessions#create'
+
+  namespace :v2 do
+    resources :monsters
+  end
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
